9eb8ece6c2a0d1bb7c8fbe90a0d103560c7744be
Sharmaine <sharmainetaylor@ymail.com>
2022-04-08 00:11:14 +0100

    Wrote a function that adds to numbers togther

---
+++ add.js
@@ -0,0 +1,9 @@
+function solution(param1, param2) {
+  console.log(param1 + param2);
+  // return param1 + param2;
+}
+
+solution(10, 25)
+solution(756, 265);
+solution(1008, 25);
+solution(75765, 1);
